0579872d97ed323b5095510741162829a5e7d9ee
Alejandro <alejandrohehheh@gmail.com>
2024-12-12 19:14:41 -0600

    gunicorn

--- miPagWeb/miPagWeb/wsgi.py
+++ miPagWeb/miPagWeb/wsgi.py
@@ -1,20 +1,9 @@
-"""
-WSGI config for  project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
-
-application = WhiteNoise(application)
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'miPagWeb.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application)  # Esta línea debería ir después de get_wsgi_application()
